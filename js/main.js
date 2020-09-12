08252053f5c633d0aef750e0cd930c437d1417fd
chai-ma1 <chaima.beldi@supcom.tn>
2020-09-12 22:17:13 +0100

    added new file and updated another

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var i=0;
+function myFunction (){
+    pop=document.getElementById("popup");
+    if(i==0)
+    {
+   
+    pop.style.display="block";
+    i=1;
+}else{
+    pop.style.display="none";
+    i=0;
+}
+    
+}
+function myFunction1 (){
+    pop=document.getElementById("popup1");
+    if(i==0)
+    {
+   
+    pop.style.display="block";
+    i=1;
+}else{
+    pop.style.display="none";
+    i=0;
+}
+    
+}
+function myFunction2(){
+    pop=document.getElementById("popup2");
+    if(i==0)
+    {
+   
+    pop.style.display="block";
+    i=1;
+}else{
+    pop.style.display="none";
+    i=0;
+}
+    
+}
+   
+function myFunction3(){
+    pop=document.getElementById("popup3");
+    if(i==0)
+    {
+   
+    pop.style.display="block";
+    i=1;
+}else{
+    pop.style.display="none";
+    i=0;
+}
+    
+}
